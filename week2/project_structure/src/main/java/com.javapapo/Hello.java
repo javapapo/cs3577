5c2a48be8cc6f35b0a79372b8dffb585be56d180
Paris Apostolopoulos <javapapo@mac.com>
2016-11-04 22:51:07 +0000

    no message

---
+++ week2/project_structure/src/main/java/com.javapapo/Hello.java
@@ -0,0 +1,10 @@
+package com.javapapo;
+
+/**
+ * Created by javapapo on 03/11/2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ week2/project_structure/src/test/java/com/javapapo/HelloTest.java
@@ -0,0 +1,7 @@
+package com.javapapo;
+
+/**
+ * Created by javapapo on 03/11/2016.
+ */
+public class HelloTest {
+}
